f750e876822e4dd1cc7e0366230b1aa245be82a2
OnoGielenballs <r1065907@student.thomasmore.be>
2025-03-26 13:29:30 +0100

    exercices

--- topics/storage.starter.js
+++ topics/storage.starter.js
@@ -4,17 +4,49 @@
  /  ___/\   __\/  _ \_  __ \__  \   / ___\_/ __ \ 
  \___ \  |  | (  <_> )  | \// __ \_/ /_/  >  ___/ 
 /____  > |__|  \____/|__|  (____  /\___  / \___  >
-     \/                         \//_____/      \/ 
+     \/                         \/_____/      \/ 
 */
 
-// 1 - click on the button, then add your name to localstorage in the key "myName"
-
-// 2 - click on the button to read the value of "myName" from localstorage and display it span#myName
-
-// 3 - click on remove button to remove "myName" from localstorage
-
-// 4 - click on the button to add a movie to the localstorage in the key "movies", show movies in the ul#movieList
-// hint: the value of movies should be an array of strings
-// hint: you can use JSON.stringify to convert an array to string
-// hint: you can use JSON.parse to convert a string to array
-// hint: you can use appendChild to add a new li to the ul#movieList
+// 1 - click on the button, then add your name to localStorage in the key "myName"
+document.querySelector("#saveName").addEventListener("click", () => {
+     localStorage.setItem("myName", "Your Name");
+ });
+ 
+ // 2 - click on the button to read the value of "myName" from localStorage and display it in span#myName
+ document.querySelector("#showName").addEventListener("click", () => {
+     const name = localStorage.getItem("myName");
+     document.querySelector("#myName").textContent = name ? name : "No name found";
+ });
+ 
+ // 3 - click on remove button to remove "myName" from localStorage
+ document.querySelector("#removeName").addEventListener("click", () => {
+     localStorage.removeItem("myName");
+     document.querySelector("#myName").textContent = "";
+ });
+ 
+ // 4 - click on the button to add a movie to localStorage in the key "movies", show movies in the ul#movieList
+ document.querySelector("#addMovie").addEventListener("click", () => {
+     const movie = prompt("Enter movie name:");
+     if (movie) {
+         let movies = JSON.parse(localStorage.getItem("movies")) || [];
+         movies.push(movie);
+         localStorage.setItem("movies", JSON.stringify(movies));
+         renderMovies();
+     }
+ });
+ 
+ // Function to render movies in the ul#movieList
+ function renderMovies() {
+     const movieList = document.querySelector("#movieList");
+     movieList.innerHTML = "";
+     const movies = JSON.parse(localStorage.getItem("movies")) || [];
+     movies.forEach(movie => {
+         const li = document.createElement("li");
+         li.textContent = movie;
+         movieList.appendChild(li);
+     });
+ }
+ 
+ // Initial rendering of movies when page loads
+ renderMovies();
+ 
--- topics/timers.starter.js
+++ topics/timers.starter.js
@@ -8,10 +8,39 @@ _/  |_|__| _____   ___________  ______
 */
 
 // 1 — start a timer and show a ⏰ in #clock after 10 seconds
+setTimeout(() => {
+  document.querySelector("#clock").textContent = "⏰";
+}, 10000);
 
 // 2 — click the start button to show a ticking clock in #timer that goes up every second
 // clicking stop should clear and stop the timer and reset the clock to 0
+let timer;
+let count = 0;
+
+document.querySelector("#start").addEventListener("click", () => {
+  timer = setInterval(() => {
+      count++;
+      document.querySelector("#timer").textContent = count;
+  }, 1000);
+});
+
+document.querySelector("#stop").addEventListener("click", () => {
+  clearInterval(timer);
+  count = 0;
+  document.querySelector("#timer").textContent = count;
+});
 
 // 3 — create an image gallery that shows a new image every 5 seconds and repeats that cycle
 // use the images gallery1.jpg, gallery2.jpg, gallery3.jpg in the images/timers_intervals folder
 // use only one img tag in the html and change its src attribute (#slideshow)
+const images = [
+  "images/timers_intervals/gallery1.jpg",
+  "images/timers_intervals/gallery2.jpg",
+  "images/timers_intervals/gallery3.jpg"
+];
+let index = 0;
+
+setInterval(() => {
+  document.querySelector("#slideshow").src = images[index];
+  index = (index + 1) % images.length;
+}, 5000);
